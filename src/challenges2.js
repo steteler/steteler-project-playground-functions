d6e2af022ca9b9457c219d45da10b1e6eafc36ef
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-05 11:15:39 -0300

    terminado desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,8 +15,29 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(index, array) {
+  let lessThanThree = true;
+  let counterOfRep = 0;
+  for (let index2 = 1; index2 < array.length; index2 += 1) {
+    if (array[index] === array[index2]) {
+      counterOfRep += 1;
+    }
+  }
+  if (counterOfRep > 2) {
+    lessThanThree = false;
+  }
+  return lessThanThree;
+}
+function lessThanThreeReps(array) {
+  let lessThanThree = true;
+  for (let index = 0; index < array.length; index += 1) {
+    lessThanThree = lessThanThreeRepsSecond(index, array);
+    if (lessThanThree === false) {
+      break;
+    }
+  }
+  return lessThanThree;
+}
 }
 
 // Desafio 12
